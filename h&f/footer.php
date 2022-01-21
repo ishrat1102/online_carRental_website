dec9e84efa711509da82782e50ea5de0398731bd
Ishrat Jaben Bushra <83410344+ishrat1102@users.noreply.github.com>
2022-01-21 13:38:31 +0600

    Add files via upload

---
+++ h&f/footer.php
@@ -0,0 +1,48 @@
+<html>
+
+<head>
+	<html>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+	<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+</head>
+
+<body>
+	<div class="container px-5 py-5" style="background-color: #6c757d;">
+		<div class="row " class="text-center">
+			<div class="col-md-3.5 offset-md-4.5">
+				<div class="full contact-full">
+					<div class="cont-top">
+						<div class="sec-title" class="text-center">
+							<h3 class="text-center" style="color:white"><b>Contact Us</b></h3>
+							<p class="text-center">
+								<i class="fas fa-map-marker-alt" style="font-size:24px;color:white"></i> <span style="color:white">Building A/1,Lane 4,Khilgaon,Dhaka,Bangladesh</span>
+							</p>
+							<p class="text-center">
+								<i class="fa fa-phone" style="font-size:24px;color:white"></i><span style="color:white "> 02-45681237,+88 01722083434</span>
+							</p>
+							<p class="text-center">
+								<i class="fa fa-envelope" style="font-size:24px;color:white"></i> <span style="color:white">ishratcarrental@gmail.com</span>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class="row mt-5 py-5">
+			<p class="text-center" style="color:white">Project done by ISHRAT JABEN BUSHRA</p><br>
+			<div class="col">
+				<p class="text-center" style="color:white"> &copy; 2021 ishratcarrental.com : Online Ishrat Car Rental Service in Dhaka,Bangladesh. All rights reserved </p>
+			</div>
+		</div>
+	</div>
+
+
+</body>
+
+</html>
---
+++ h&f/loginhead.php
@@ -0,0 +1,59 @@
+<html>
+
+<head>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+    body {
+
+      background-color: white;
+      color: black;
+      text-align: center;
+      border: 40px solid #6c757d;
+      margin: 0px;
+    }
+  </style>
+  </style>
+</head>
+
+<body style="background-color:#6c757d;">
+  <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#" style="color:white">Ishrat Car Rental</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="homepage.php" style="color:white"><i class="fa fa-home"></i>Home</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a style="color:white" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="fa fa-reorder"></i> Options
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="carlist_user.php">All Cars</a></li>
+              <li><a class="dropdown-item" href="give_rating.php">Give Rating</a></li>
+              <li><a class="dropdown-item" href="view_your_rating.php">View Your Rating</a></li>
+              <li><a class="dropdown-item" href="view_all_rating.php">View All Ratings</a></li>
+              <li><a class="dropdown-item" href="view_your_renting.php">View Your Renting</a></li>
+              <li><a class="dropdown-item" href="profile.php">Profile Page</a></li>
+              <li><a class="dropdown-item" href="logout.php">logout</a></li>
+
+            </ul>
+          </li>
+
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+
+</body>
+
+</html>
